c0b4826d1487add5332b786903dbc067a7814b88
Hesham Ayad <124933911+hesham-ayad@users.noreply.github.com>
2023-12-28 13:31:44 +0200

    reverted to original purpose

--- src/index.js
+++ src/index.js
@@ -4,47 +4,47 @@ import mcJsoData from './geoJson/mcdonaldsPerState.json';
 import usStatesJsoData from './geoJson/usStatesCoo.json';
 
 
-// map.on('load', () => {
-//   addUsaStates();
-// });
+map.on('load', () => {
+  addUsaStates();
+});
 
-// function addUsaStates() {
-//   map.addSource('states', {
-//     type: 'geojson',
-//     data: usStatesJsoData,
-//   });
+function addUsaStates() {
+  map.addSource('states', {
+    type: 'geojson',
+    data: usStatesJsoData,
+  });
 
-//   map.addLayer({
-//     id: 'states-fill-layer',
-//     type: 'fill',
-//     source: 'states',
-//     paint: {
-//       'fill-opacity': 0.2,
-//       'fill-color': '#00FF00',
-//     },
-//   });
+  map.addLayer({
+    id: 'states-fill-layer',
+    type: 'fill',
+    source: 'states',
+    paint: {
+      'fill-opacity': 0.2,
+      'fill-color': '#00FF00',
+    },
+  });
 
-//   map.addLayer({
-//     id: 'states-line-layer',
-//     type: 'line',
-//     source: 'states',
-//     paint: {
-//       'line-opacity': 0.2,
-//       'line-color': '#000000',
-//     },
-//   });
-// }
+  map.addLayer({
+    id: 'states-line-layer',
+    type: 'line',
+    source: 'states',
+    paint: {
+      'line-opacity': 0.2,
+      'line-color': '#000000',
+    },
+  });
+}
 
-// const bounds = bbox(usStatesJsoData);
-// map.fitBounds(bounds);
+const bounds = bbox(usStatesJsoData);
+map.fitBounds(bounds);
 
-// map.on('click', 'states-fill-layer', (e) => {
-//   const clickedFeature = map.queryRenderedFeatures(e.point, {
-//     layers: ['states-fill-layer'],
-//   });
+map.on('click', 'states-fill-layer', (e) => {
+  const clickedFeature = map.queryRenderedFeatures(e.point, {
+    layers: ['states-fill-layer'],
+  });
 
-//   popUp
-//     .setLngLat(e.lngLat)
-//     .setHTML(`This is ${clickedFeature[0]?.properties.name}`)
-//     .addTo(map);
-// });
+  popUp
+    .setLngLat(e.lngLat)
+    .setHTML(`This is ${clickedFeature[0]?.properties.name}`)
+    .addTo(map);
+});
--- src/map.js
+++ src/map.js
@@ -9,9 +9,30 @@ MapLibreGL.setRTLTextPlugin(
   true
 );
 
-export const map = new MapLibreGL.Map({
+export const map = new maplibregl.Map({
   container: 'map', // container id
-  style: '/cairo-mbtiles-style.json',
-  center: [31.2863, 30.0431],
-  zoom: 9,
+  style: {
+    version: 8,
+    glyphs: '/fonts/{fontstack}/{range}.pbf',
+    sources: {
+      'raster-tiles': {
+        type: 'raster',
+        tiles: ['https://tile.openstreetmap.org/{z}/{x}/{y}.png'],
+        tileSize: 256,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      },
+    },
+    layers: [
+      {
+        id: 'simple-tiles',
+        type: 'raster',
+        source: 'raster-tiles',
+        minzoom: 0,
+        maxzoom: 22,
+      },
+    ],
+  },
+  center: [0, 0],
+  zoom: 1.5,
 });
